b3ca5b01e731a12068c2b05da282f60119d0c3e7
mestraub <megan@wdwstraubs.com>
2014-03-18 17:58:35 -0400

    BST Project starting to happen

---
+++ BinarySearchTree.java
@@ -0,0 +1,99 @@
+package proj2;
+
+public class BinarySearchTree<AnyType extends Comparable <? super AnyType>> {
+
+	/**
+	 * need these functions:
+	 * 	insert
+	 * 	printRoot
+	 * 	printTree
+	 * 
+	 * 	void findCommonAncestor(AnyType x, AnyType y)
+	 */
+	
+	private BinaryNode<AnyType> root;
+	
+	/**
+	 * 
+	 */
+	public BinarySearchTree(){
+		root = null;
+	}
+	
+	public void createEmpty(){
+		root = null;
+	}
+	
+	public boolean isEmpty(){
+		return root == null;
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	public void insert(AnyType x){
+		root = insert (x, root);
+	}
+	
+	public BinaryNode<AnyType> (AnyType x, BinaryNode<AnyType> t){
+		if ( t == null)
+			return new BinaryNode<>(x, null, null);
+		
+		int compareResult = x.compareTo(t.data);
+		
+		if (compareResult < 0)
+			t.left = insert (x, t.left);
+		else if (compareResult > 0)
+			t.right = insert (x, t.right);
+		else
+			; // duplicate find out what to do
+		
+		return t;
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void printRoot(){
+		System.out.println(root);
+	}
+	
+	/**
+	 * 
+	 */
+	public void printTree(){
+		
+	}
+	
+	private static class BinaryNode<AnyType>{
+		// page 113
+		AnyType data; // the data in the node
+		BinaryNode<AnyType> left; // left child
+		BinaryNode<AnyType> right; // right child
+		
+		/**
+		 * 
+		 */
+		BinaryNode(AnyType theNode){
+			this(theNode,null,null);
+		}
+		
+		/**
+		 * 
+		 */
+		BinaryNode(AnyType theNode, BinaryNode<AnyType> leftNode, BinaryNode<AnyType> rightNode){
+			data = theNode;
+			left = leftNode;
+			right = rightNode;
+		}
+		
+		AnyType getData(){
+			return data;
+		}
+		
+	}// end binarynode class
+	
+}
---
+++ HashedBSTs.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package proj2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Megan
+ *
+ */
+public class HashedBSTs <AnyType>{
+
+	//private ArrayList<BinarySearchTree> table;
+	
+	/**
+	 * a constructor that will accept the size of the hashed table
+	 */
+	public HashedBSTs(int size){
+		// create array list called table
+		//table = new ArrayList
+	}
+	
+	/**
+	 * this is a grading and debugging tool
+	 * should run in O(n) time
+	 */
+	public void printHashCountsResults(){
+		
+	}
+	
+	/**
+	 * will open a file, filter it to the distinct words, remove ALL punction, and numbers
+	 * then place those words into the apprpriate BST in the arraylist table
+	 * 
+	 * should run in O(n) time
+	 */
+	
+	public void fileReader(String fileName){
+		
+		File textFile = new File(fileName);
+		
+		String str;
+		
+		try {
+			// reads in the text file
+			Scanner scanFile = new Scanner(textFile);
+			
+			while (scanFile.hasNext()){
+				str = scanFile.next();
+				System.out.println(str);
+			}
+			
+			scanFile.close();
+			
+		}// end try
+		catch (FileNotFoundException e){	
+			
+			System.out.println("File not found.");
+			e.printStackTrace();
+		}// end catch
+	}
+	
+	/**
+	 * will collect all codes that START with the letters in teh sample Node and print that list of nodes
+	 * 
+	 * should run O(lgn) time
+	 */
+	
+	public void findAll(BinarySearchTree<Node> retrieved, Node sample){
+		
+	}
+	
+	
+	
+	/**
+	 * letters 'A' and 'a' will be at index 0
+	 * letters 'B' and 'b' will be at index 1
+	 * there will be 26 spots in the table
+	 * 
+	 * use an array list to hold the BST called table, teh size will be 26
+	 */
+	
+}
